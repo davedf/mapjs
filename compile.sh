f1305a6c5f896e61307b403bbabfb51583b922bd
Damjan Vujnovic <damjan@mini.local>
2013-01-18 11:59:43 +0000

    more bash awesomeness

--- compile.sh
+++ compile.sh
@@ -1,6 +1,4 @@
-if [ ! -d "pub" ]; then
- mkdir pub
-fi
+mkdir -p pub
 cat src/observable.js src/content.js src/layout.js src/map-model.js src/kinetic.connector.js src/kinetic.idea.js src/kinetic-mediator.js > pub/mapjs-compiled.js
 if [ -d "../mindmup/public" ]; then
  cp pub/mapjs-compiled.js ../mindmup/public
