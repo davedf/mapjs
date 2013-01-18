c5e1fe02b6cd3fd8589f918e0a8ff25c1680ec01
Damjan Vujnovic <damjan@mini.local>
2013-01-18 10:57:37 +0000

    pub directory created if missing

--- compile.sh
+++ compile.sh
@@ -1,3 +1,6 @@
+if [ ! -d "pub" ]; then
+ mkdir pub
+fi
 cat src/observable.js src/content.js src/layout.js src/map-model.js src/kinetic.connector.js src/kinetic.idea.js src/kinetic-mediator.js > pub/mapjs-compiled.js
 if [ -d "../mindmup/public" ]; then
  cp pub/mapjs-compiled.js ../mindmup/public
