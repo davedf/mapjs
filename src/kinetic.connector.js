86f176f3ea2f19ea8120215a66b00eed506e98c1
davedf <dave@deflorinier.me.uk>
2013-01-19 08:21:15 +0000

    Alter logic for switching between side and top and bottom connectors

--- src/kinetic.connector.js
+++ src/kinetic.connector.js
@@ -25,12 +25,12 @@
 			controlPointOffset: 0
 		};
 	};
-	calculateConnector = function (parent, child, ctrl) {
+	calculateConnector = function (parent, child) {
 		var tolerance = 10,
 			childMid = child.attrs.y + child.getHeight() * 0.5,
 			parentMid = parent.attrs.y + parent.getHeight() * 0.5,
 			childHorizontalOffset;
-		if (Math.abs(parentMid - childMid) < Math.min(child.getHeight(), parent.getHeight()) * 0.75) {
+		if (Math.abs(parentMid - childMid) + tolerance < Math.max(child.getHeight(), parent.getHeight()) * 0.75) {
 			return horizontalConnector(parent, child);
 		}
 		childHorizontalOffset = parent.attrs.x < child.attrs.x ? 0 : 1;
@@ -51,8 +51,7 @@
 			var context = this.getContext(),
 				shapeFrom = this.shapeFrom,
 				shapeTo = this.shapeTo,
-				ctrl = 0.2,
-				conn = calculateConnector(shapeFrom, shapeTo, ctrl),
+				conn = calculateConnector(shapeFrom, shapeTo),
 				offset,
 				maxOffset;
 			if (!conn) {
